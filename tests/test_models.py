b9f48073cc937cf516789b8328d671326135fd21
Jacob Walls <jwalls@fargeo.com>
2024-07-30 17:41:22 -0400

    Add coverage re #8

---
+++ tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from arches_references.models import ListItem
+
+# these tests can be run from the command line via
+# python manage.py test tests.test_models --settings="tests.test_settings"
+
+
+class ListItemTests(TestCase):
+    def test_uri_generation_guards_against_failure(self):
+        # Don't bother setting up a list.
+        item = ListItem(sortorder=0)
+        item.id = None
+
+        with self.assertRaises(RuntimeError):
+            item.clean()
+
+        item.full_clean(exclude={"list"})
+        self.assertIsNotNone(item.uri)
