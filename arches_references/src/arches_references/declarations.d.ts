5858ba1474cd9f7a0cdfff2ebe406de7a9bf9bcf
Jacob Walls <jwalls@fargeo.com>
2024-09-04 10:33:16 -0400

    Export types

--- arches_references/src/arches_references/declarations.d.ts
+++ arches_references/src/arches_references/declarations.d.ts
@@ -2,3 +2,9 @@
 // Module homepage on npmjs.com uses logos "TS" or "DT" to indicate if typed
 
 import("@/arches/declarations.d.ts");
+
+export * from "./api";
+export * from "./constants";
+export * from "./routes";
+export * from "./types";
+export * from "./utils";
