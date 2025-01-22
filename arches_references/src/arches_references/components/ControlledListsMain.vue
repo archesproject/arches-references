419831821cd31975df332cb3c2d898de49b2706e
Jacob Walls <jwalls@fargeo.com>
2025-01-22 14:59:14 -0500

    Remove calc

--- arches_references/src/arches_references/components/ControlledListsMain.vue
+++ arches_references/src/arches_references/components/ControlledListsMain.vue
@@ -54,8 +54,7 @@ provide(systemLanguageKey, systemLanguage);
 </script>
 
 <template>
-    <!-- Subtract size of arches toolbars -->
-    <div style="width: calc(100vw - 50px); height: calc(100vh - 50px)">
+    <div style="width: 100vw; height: 100vh; padding-bottom: 1.75rem">
         <div class="list-editor-container">
             <ListHeader />
             <MainSplitter />
